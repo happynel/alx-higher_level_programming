8184a031139410f30f090c7281bad2cf75b53912
happynel <frankyhappy5050@gmail.com>
2023-05-18 04:29:44 -0700

    Delete database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creat databses
+-- Create a databses if it does not exists
+CREATE A DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Delete a database
+-- Delete a database if it does not exists
+DELETE DATABASE IF NOT EXISTS hbtn_0c_0;
