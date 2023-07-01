42bb8d46672bdbc569f951905ea7b1baf364bcae
happynel <frankyhappy5050@gmail.com>
2023-07-01 18:22:15 +0000

    Network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# send a DELETE request to an URL with curl and display the body of the response
+curl -sX DELETE "$1"
